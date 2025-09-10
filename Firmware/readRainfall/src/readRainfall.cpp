c59bc3d9e046343681d5ade492ad3ceaa24fac7a
Bentley Settin <bfs7357@uncw.edu>
2025-09-10 13:23:21 -0400

    update readrainfall2

--- Firmware/readRainfall/src/readRainfall.cpp
+++ Firmware/readRainfall/src/readRainfall.cpp
@@ -1,40 +1,40 @@
-//-------------Adapted from Particle Boron Logic------------------//
-#include "Particle.h"
+// //-------------Adapted from Particle Boron Logic------------------//
+// #include "Particle.h"
 
-// Rainfall sensor via digital tipping bucket
-const int RAIN_PIN = D2;        // digital pin connected to tipping bucket
-volatile uint32_t rainfall_tips = 0;
-float rainfall_mm = 0.0;
-const float MM_PER_TIP = 0.2794; // mm per tip
+// // Rainfall sensor via digital tipping bucket
+// const int RAIN_PIN = D2;        // digital pin connected to tipping bucket
+// volatile uint32_t rainfall_tips = 0;
+// float rainfall_mm = 0.0;
+// const float MM_PER_TIP = 0.2794; // mm per tip
 
-// ISR triggered on tipping bucket
-void rainfallISR() {
-    rainfall_tips++;
-}
+// // ISR triggered on tipping bucket
+// void rainfallISR() {
+//     rainfall_tips++;
+// }
 
-void setup() {
-    Serial.begin(115200);
-    Serial1.begin(9600);
+// void setup() {
+//     Serial.begin(115200);
+//     Serial1.begin(9600);
 
-    // LED pin
-   // pinMode(led, OUTPUT);
+//     // LED pin
+//    // pinMode(led, OUTPUT);
 
-    // Rainfall sensor ISR
-    pinMode(RAIN_PIN, INPUT_PULLUP);
-    attachInterrupt(RAIN_PIN, rainfallISR, FALLING);
+//     // Rainfall sensor ISR
+//     pinMode(RAIN_PIN, INPUT_PULLUP);
+//     attachInterrupt(RAIN_PIN, rainfallISR, FALLING);
 
-    Log.info("Setup complete");
-}
+//     Log.info("Setup complete");
+// }
 
-void loop() {
-    // Compute total rainfall
-    rainfall_mm = rainfall_tips * MM_PER_TIP;
+// void loop() {
+//     // Compute total rainfall
+//     rainfall_mm = rainfall_tips * MM_PER_TIP;
 
-    // Log rainfall
-    Log.info("Rainfall: %.2f mm", rainfall_mm);
+//     // Log rainfall
+//     Log.info("Rainfall: %.2f mm", rainfall_mm);
     
-    delay(1000);
-}
+//     delay(1000);
+// }
 
 
 
--- Firmware/readRainfall/src/readRainfall2.cpp
+++ Firmware/readRainfall/src/readRainfall2.cpp
@@ -1,38 +1,54 @@
-// //------------ DFRobot rainfall read adapted to Particle Boron--------//
+//------------ DFRobot rainfall read adapted to Particle Boron--------//
 
-// #include "Particle.h"
-// #include "DFRobot_RainfallSensor.h"
+#include "Particle.h"
+#include "DFRobot_RainfallSensor.h"
 
-// // Pointer for the UART rainfall sensor (???)
-// DFRobot_RainfallSensor_UART* rainfallSensor;
+// Pointer for the UART rainfall sensor (???)
+DFRobot_RainfallSensor_UART* rainfallSensor;
 
-// void setup() {
-//     Serial1.begin(9600);
-//     delay(1000); 
+void setup() {
+    Serial.begin(9600);
+    Serial1.begin(9600);
 
-//     Log.info("Starting rainfall sensor setup...");
+    delay(1000); 
+
+    Log.info("Starting rainfall sensor setup...");
 
     
-//     rainfallSensor = new DFRobot_RainfallSensor_UART(Serial1);
+    rainfallSensor = new DFRobot_RainfallSensor_UART(Serial1);
 
-//     // Attempt sensor initialization
-//     while (!rainfallSensor->begin()) {
-//         Log.info("Rainfall sensor init failed (UART)!");
-//         delay(3000); 
-//     }
+    // Attempt sensor initialization
+    while (!rainfallSensor->begin()) {
+        Log.info("Rainfall sensor init failed (UART)!");
+        delay(3000); 
+    }
 
-//     Log.info("Rainfall sensor initialized successfully!");
-//     Log.info("Firmware version: %s", rainfallSensor->getFirmwareVersion().c_str());
+    Log.info("Rainfall sensor initialized successfully!");
+    Log.info("Firmware version: %s", rainfallSensor->getFirmwareVersion().c_str());
 
-//     delay(1000);
-// }
+    delay(1000);
+}
 
-// void loop() {
+void loop() {
 
-//     Log.info("Rainfall total: %.2f mm", rainfallSensor->getRainfall());
-//     Log.info("Sensor working time: %.2f h", rainfallSensor->getSensorWorkingTime());
-//     Log.info("Raw bucket counts: %lu", rainfallSensor->getRawData());
-//     Log.info("-----------------------------");
+    // Log.info("Rainfall total: %.2f mm", rainfallSensor->getRainfall());
+    // Log.info("Sensor working time: %.2f h", rainfallSensor->getSensorWorkingTime());
+    // Log.info("Raw bucket counts: %lu", rainfallSensor->getRawData());
+    // Log.info("-----------------------------");
 
-//     delay(1000);
-// }
+    // delay(1000);
+
+  //Get the sensor working time, unit: hour
+  Log.info("Sensor WorkingTime:\t");
+  Log.info("%lu H", rainfallSensor->getSensorWorkingTime());
+  //Get the accumulated rainfall during the sensor working time
+  Log.info("Rainfall:\t");
+  Log.info("%.2f mm", rainfallSensor->getRainfall());
+  //Get the accumulated rainfall within 1 hour of the system (function parameter optional 1-24)
+  Log.info("1 Hour Rainfall:\t");
+  Log.info("%.2f mm", rainfallSensor->getRainfall(1));
+  //Get the raw data, the number of tipping buckets for rainfall, unit: times
+  Log.info("rainfall raw:\t");
+  Log.info("%lu", rainfallSensor->getRawData());
+  delay(1000);
+}
