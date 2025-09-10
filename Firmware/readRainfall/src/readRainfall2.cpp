acca569cedca998bff60d37c862d80c7cbf62b21
Bentley Settin <bfs7357@uncw.edu>
2025-09-10 13:57:41 -0400

    added checkmarks in readrainfall2

--- Firmware/readRainfall/src/readRainfall2.cpp
+++ Firmware/readRainfall/src/readRainfall2.cpp
@@ -3,52 +3,30 @@
 #include "Particle.h"
 #include "DFRobot_RainfallSensor.h"
 
-// Pointer for the UART rainfall sensor (???)
-DFRobot_RainfallSensor_UART* rainfallSensor;
+DFRobot_RainfallSensor_UART Sensor(Serial1);  // Pass Serial1 by reference, not pointer
+
 
 void setup() {
-    Serial.begin(9600);
+    Log.info("Setup started");
     Serial1.begin(9600);
 
-    delay(1000); 
-
-    Log.info("Starting rainfall sensor setup...");
-
-    
-    rainfallSensor = new DFRobot_RainfallSensor_UART(Serial1);
-
-    // Attempt sensor initialization
-    while (!rainfallSensor->begin()) {
-        Log.info("Rainfall sensor init failed (UART)!");
-        delay(3000); 
+    while(!Sensor.begin()) {
+        Log.error("Sensor init err!!!");
+        delay(1000);
     }
 
-    Log.info("Rainfall sensor initialized successfully!");
-    Log.info("Firmware version: %s", rainfallSensor->getFirmwareVersion().c_str());
-
-    delay(1000);
+    Log.info("Version:\t");
+    Log.info(Sensor.getFirmwareVersion());
 }
 
 void loop() {
 
-    // Log.info("Rainfall total: %.2f mm", rainfallSensor->getRainfall());
-    // Log.info("Sensor working time: %.2f h", rainfallSensor->getSensorWorkingTime());
-    // Log.info("Raw bucket counts: %lu", rainfallSensor->getRawData());
-    // Log.info("-----------------------------");
+    Log.info("Sensor WorkingTime: %lu H", Sensor.getSensorWorkingTime());
+    Log.info("Rainfall: %.2f mm", Sensor.getRainfall());
+    Log.info("1 Hour Rainfall: %.2f mm", Sensor.getRainfall(1));
+    Log.info("Raw Tipping Counts: %lu", Sensor.getRawData());
 
-    // delay(1000);
-
-  //Get the sensor working time, unit: hour
-  Log.info("Sensor WorkingTime:\t");
-  Log.info("%lu H", rainfallSensor->getSensorWorkingTime());
-  //Get the accumulated rainfall during the sensor working time
-  Log.info("Rainfall:\t");
-  Log.info("%.2f mm", rainfallSensor->getRainfall());
-  //Get the accumulated rainfall within 1 hour of the system (function parameter optional 1-24)
-  Log.info("1 Hour Rainfall:\t");
-  Log.info("%.2f mm", rainfallSensor->getRainfall(1));
-  //Get the raw data, the number of tipping buckets for rainfall, unit: times
-  Log.info("rainfall raw:\t");
-  Log.info("%lu", rainfallSensor->getRawData());
-  delay(1000);
+    delay(1000);
 }
+
+
