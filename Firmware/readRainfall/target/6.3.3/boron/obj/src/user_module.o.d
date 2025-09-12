d960e88b33fb9869a66448d42637a694e539b88d
Bentley Settin <bfs7357@uncw.edu>
2025-09-12 09:30:14 -0400

    attempt to show libraries in gh

---
+++ Firmware/readRainfall/target/6.3.3/boron/obj/src/user_module.o.d
@@ -0,0 +1,7 @@
+c\:/Users/ppita/Documents/Particle/Open-Water-Level-and-Rainfall-Sensor/Firmware/readRainfall/target/6.3.3/boron//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h ../../../hal/shared/platforms.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
+../../../hal/shared/platforms.h:
